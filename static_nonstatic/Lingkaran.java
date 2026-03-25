e7e8f1ffa75282d7a8b9857a831bbe8707247725
Angela Mery Rosarie Castilla Pathia Ratri Kirana <angelajolie2017@gmail.com>
2026-03-25 21:53:23 +0700

    first commit

---
+++ static_nonstatic/Lingkaran.java
@@ -0,0 +1,19 @@
+package static_nonstatic;
+
+public class Lingkaran {
+
+    public static float PI = 3.14f;
+    public float r;
+
+    public Lingkaran(float r) {
+        this.r = r;
+    }
+
+    public float luas() {
+        return Lingkaran.PI * r * r;
+    }
+    
+    public static float keliling(float r) {
+        return 2 * PI * r;
+    }
+}
