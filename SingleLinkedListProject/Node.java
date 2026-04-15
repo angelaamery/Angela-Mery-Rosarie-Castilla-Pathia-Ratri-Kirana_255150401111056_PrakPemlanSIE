b5dc50cab9bdad5eab4d44fd1bb3a142d8194e6d
Angela Mery Rosarie Castilla Pathia Ratri Kirana <angelajolie2017@gmail.com>
2026-04-15 17:12:53 +0700

    Modul 6

---
+++ SingleLinkedListProject/Node.java
@@ -0,0 +1,25 @@
+package SingleLinkedListProject;
+
+public class Node {
+    Object data;
+    Node next;
+
+    Node(Object data) {
+        this.data = data;
+    }
+
+      @Override
+    public String toString() {
+        return String.valueOf(data);
+    }
+
+    public static void main(String[] args) {
+        Node node = new Node();
+        node.data = "A";
+
+        System.out.println("node    : " + node);
+        System.out.println("data    : " + node.data);
+        System.out.println("pointer : " + node.next);
+
+    }
+}
